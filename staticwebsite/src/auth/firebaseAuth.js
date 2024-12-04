4a363b5c580e79e603dc9df0416405ff329f8a6e
clamba15 <cl@customcraftsoftware.com>
2024-12-03 23:59:22 -0800

    static

--- staticwebsite/src/auth/firebaseAuth.js
+++ staticwebsite/src/auth/firebaseAuth.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from './firebase';
 
 // Login function
